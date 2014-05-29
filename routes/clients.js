617b9ef22e19c293d268567645dd91eba6028a38
Linda Parsons <lparsonstech@gmail.com>
2014-05-29 01:50:45 +0000

    Added code to run anon_client python script

--- routes/clients.js
+++ routes/clients.js
@@ -5,16 +5,21 @@ exports.anon = function(req, res) {
   console.log('In anon server call');
   var python = spawn(
     'python',
-    ['anon_client --debug --verbose --server rabbitmq.prisem.washington.edu --queue-base anon --stats --file data/rwfind_201210011617_8428.txt']
+    ['/opt/dims/bin/anon_client', '--debug', '--verbose', '--server', 'rabbitmq.prisem.washington.edu',
+        '--queue-base', 'anon', '--stats', '--file', 'data/rwfind_201210011617_8428.txt']
     );
   var output = '';
-  python.stdout.on('data', function() {
+  python.stdout.on('data', function(data) {
     output += data
-    console.log(data);
+    console.log('stdout: '+ data);
+  });
+
+  python.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
   });
   python.on('close', function(code) {
     if (code !== 0) {
-      return res.send(500, code);
+      return res.send(500, code, output);
     }
     return res.send(200, output);
   })
