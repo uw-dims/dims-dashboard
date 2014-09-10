95878f6a960bfa86293d67593fadf134434038d2
Linda Parsons <lparsonstech@gmail.com>
2014-09-10 07:55:45 -0400

    Updated jasmine test suite dependency, updated jasmine test for controller RwfindCtrl

--- client/test/spec/controllers/rwfindCtrl.js
+++ client/test/spec/controllers/rwfindCtrl.js
@@ -1,6 +1,7 @@
 'use strict';
 
-describe('Controller: RwfindCtrl', function () {
+//Jasmine Suite
+describe('Suite testing Controller: RwfindCtrl', function () {
 
   // load the controller's module
   beforeEach(module('dimsDashboard'));
@@ -16,7 +17,17 @@ describe('Controller: RwfindCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  //Jasmine spec
+  it('should initialize scope properties', function () {
+    expect(scope.showResults).toBe(false);
+    expect(scope.showJsonResults).toBe(false);
+    expect(scope.resultsMsg).toBe('Results');
+    expect(scope.result).toBeNull();
+    expect(scope.rawData).toBe('');
+    expect(scope.dataSize).toBe(100);
+    expect(scope.totalDataSize).toBe(0);
+    expect(scope.query).toBe('');
+    expect(scope.flows.length).toBe(0);
+    expect(scope.flowStats.length).toBe(0);
   });
 });
