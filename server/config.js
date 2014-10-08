d075e9d480b99f1a20bcae838c8233b0fbf8da65
Linda Parsons <lparsonstech@gmail.com>
2014-10-08 16:57:58 -0400

    Changing config for web application to use cifbulk queue base cifbulk_v1_demo_test for testing purposes

--- server/config.js
+++ server/config.js
@@ -47,7 +47,7 @@ config.defaultMapping = {
 };
 
 config.rpcQueueNames = {
-  'cifbulk': 'cifbulk_v1_test',
+  'cifbulk': 'cifbulk_v1_demo_test',
   'anon': 'anon',
   'rwfind': 'rwfind',
   'crosscor': 'crosscor'
