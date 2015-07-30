e53e9616b6231c2ca2b806012bd830b194286362
Linda Parsons <lparsonstech@gmail.com>
2015-07-30 10:45:40 -0400

    Reverted test so it passes

--- server/test/spec/models/keyGen.js
+++ server/test/spec/models/keyGen.js
@@ -72,7 +72,7 @@ test('models/keyGen.js: keyGen should return keys for files', function (assert) 
   assert.equal(keyGen.fileKey(fileObject2), fileKey2, 'Returns correct file key');
   assert.equal(keyGen.fileSetKey(fileObject1), fileSetKey1, 'Returns correct file set key');
   assert.equal(keyGen.fileSetKey(fileObject2), fileSetKey2, 'Returns correct file set key');
-  assert.equal(keyGen.fileMetaKey(fileObject1), fileMetaKey2, 'Returns correct file metadata key');
+  assert.equal(keyGen.fileMetaKey(fileObject1), fileMetaKey1, 'Returns correct file metadata key');
   assert.equal(keyGen.fileMetaKey(fileObject2), fileMetaKey2, 'Returns correct file metadata key');
   assert.end();
 });
