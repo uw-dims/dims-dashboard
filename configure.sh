42c1bc7928c7bb5a88c6bda9c0108438d331de70
Linda Parsons <lparsonstech@gmail.com>
2015-11-06 10:45:45 -0500

    bumped version

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-VERSION=1.0.5
+VERSION=1.0.6
 
 # Used during development
 # Set up common functions and variables.
--- jenkins.run.tests.sh
+++ jenkins.run.tests.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-VERSION=1.0.5
+VERSION=1.0.6
 
 # Run the server tests on Jenkins
 
