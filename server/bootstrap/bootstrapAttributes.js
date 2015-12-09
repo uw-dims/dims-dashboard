ba649a86890c41218d6b791ac395ea599e9597c9
Linda Parsons <linda.parsons@nextcentury.com>
2015-12-09 13:23:56 -0500

    Bootstrap attributes now inserts usernames from data store

--- server/bootstrap/bootstrapAttributes.js
+++ server/bootstrap/bootstrapAttributes.js
@@ -21,9 +21,11 @@ bluebird.promisifyAll(client.multi());
 diContainer.factory('Attributes', require(path.join(ROOT_DIR, '/models/attributes')));
 diContainer.factory('attributeService', require(path.join(ROOT_DIR, '/services/attributes')));
 diContainer.register('client', client);
+diContainer.factory('UserModel', require(path.join(ROOT_DIR, '/models/user')));
 
 var Attributes = diContainer.get('Attributes');
 var attributeService = diContainer.get('attributeService');
+var UserModel = diContainer.get('UserModel');
 console.log('ROOT_DIR is %s', ROOT_DIR);
 
 (function () {
@@ -32,6 +34,7 @@ console.log('ROOT_DIR is %s', ROOT_DIR);
 
   exports.runBootstrap = bootstrapAttributes.runBootstrap = function () {
     console.log('Running bootstrapAttributes');
+    var users = [];
     var yamlPath = path.join(__dirname, './userAttributes.yml');
     var doc;
     try {
@@ -45,19 +48,32 @@ console.log('ROOT_DIR is %s', ROOT_DIR);
       console.error('Cannot read file at ' + yamlPath + '. Error: ', err);
       return;
     }
-    var attrPromises = [];
-    _.forEach(doc, function (config, user) {
-      var newConfig = {};
-      _.forEach(config, function (value, type) {
-        // We don't push the name field to this config
-        if (type !== 'name') {
-          newConfig[type] = value;
-        }
+
+    return UserModel.Users.forge().fetch()
+    .then(function (collection) {
+      _.forEach(collection.toJSON(), function (value, key) {
+        users.push(value.ident);
       });
-      var attributes = Attributes.attributesFactory(user);
-      attrPromises.push(attributes.updateAttributes(user, newConfig));
-    });
-    return q.all(attrPromises)
+      // Now have array of actual usernames
+      var attrPromises = [];
+      var i = 0;
+      _.forEach(doc, function (config, user) {
+        var newConfig = {};
+        _.forEach(config, function (value, type) {
+          // We don't push the name field to this config
+          if (type !== 'name') {
+            newConfig[type] = value;
+          }
+        });
+        // Replace test users - there are 4
+        var thisUser = users[i];
+        i++;
+        var attributes = Attributes.attributesFactory(thisUser);
+        attrPromises.push(attributes.updateAttributes(thisUser, newConfig));
+
+      });
+      return q.all(attrPromises);
+    })
     .then(function () {
       // print the result, save to a new file, and quit redis client
       Attributes.getAllAttributes()
